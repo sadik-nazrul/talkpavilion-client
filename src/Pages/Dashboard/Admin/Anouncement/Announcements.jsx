6916bae1c8e2f8d6a913187cd2e6235519863603
sadikul islam <sadikulislam@sadikuls-MacBook-Air.local>
2024-08-29 20:50:25 +0600

    All anouncements page creation done

--- src/Pages/Dashboard/Admin/Anouncement/Announcements.jsx
+++ src/Pages/Dashboard/Admin/Anouncement/Announcements.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import PageTitle from "../../../../Components/PageTitle";
+import useAxiosSecure from "../../../../Hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+import NodataFound from "../../../../Components/Shared/NodataFound";
 
 const Announcements = () => {
+  const axioSecure = useAxiosSecure();
+  const { data: announcements = [] } = useQuery({
+    queryKey: ["announcements"],
+    queryFn: async () => {
+      const { data: announcement } = await axioSecure.get("/announcements");
+      return announcement;
+    },
+  });
+
   return (
     <div className="overflow-x-auto">
       <PageTitle title={"Manage Users"} />
-      {users.length === 0 ? (
+      {announcements.length === 0 ? (
         <NodataFound />
       ) : (
         <table className="table table-zebra">
@@ -12,20 +25,28 @@ const Announcements = () => {
           <thead>
             <tr className="bg-orange-400 text-white">
               <th>#</th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Action</th>
-              <th>Status</th>
+              <th>Author</th>
+              <th>Title</th>
+              <th>Description</th>
+              <th>Creation Time</th>
             </tr>
           </thead>
           <tbody>
-            {users.map((user, idx) => (
-              <tr key={user._id} className="bg-gray-200">
+            {announcements.map((announcement, idx) => (
+              <tr key={announcement._id} className="bg-gray-200">
                 <th>{idx + 1}</th>
-                <td className="capitalize">{user?.name}</td>
-                <td>{user.email}</td>
-                <td>Make Admin</td>
-                <td className="capitalize">{user?.role}</td>
+                <td className="capitalize">{announcement?.authorName}</td>
+                <td>{announcement?.title}</td>
+                <td title={announcement.description}>
+                  {announcement?.description.substring(0, 24)}...
+                </td>
+                {announcement?.createdAt
+                  ? new Date(announcement.createdAt).toLocaleString("en-US", {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    })
+                  : "Not Found"}
               </tr>
             ))}
           </tbody>
